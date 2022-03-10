87b6eddc324408e15b253523bc710f14db81f33c
Данил <@DESKTOP-U9PJUQE>
2022-03-10 21:50:18 +0700

    Добавьте файлы проекта.

---
+++ VP_lab4/Models/RomanNumberCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VP_lab4.Models
+{
+    public static class RomanNumberCalculator
+    {
+        public static RomanNumberExtend Calculate(string input)
+        {
+            var splittedInputByOperands = Regex.Split(input, @"([*()\^\/]|(?<!E)[\+\-])");
+
+            string output = "";
+            foreach (var element in splittedInputByOperands)
+            {
+                if (element != "+" && element != "-" && element != "*" && element != "/")
+                {
+                    if (!RomanNumberExtend.CheckString(element)) throw new RomanNumberException("Invalid input");
+                    output += (new RomanNumberExtend(element)).UshortValue().ToString();
+                }
+                else output += element;
+            }
+            DataTable dt = new();
+            return new RomanNumberExtend(Convert.ToUInt16(dt.Compute(output, "")));
+        }
+    }
+}
---
+++ VP_lab4/Models/RomanNumberException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VP_lab4.Models
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException() : base("ERROR") { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
